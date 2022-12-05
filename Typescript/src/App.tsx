d01a8c831012cbd2544b23a388ae64fe31d958af
Kim-EunSu <kes73640@gmail.com>
2022-12-05 15:27:30 +0900

    1205

--- Typescript/src/App.tsx
+++ Typescript/src/App.tsx
@@ -1,32 +1,32 @@
+import { eventNames } from "process";
 import React, { useState } from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+// prop에 기본값 주는 방법: active = false
+function Dummy({ text, active = false }: DummyProps) {
+  return <h1>{text}</h1>;
+}
+
+// active는 active={true}와 같은 말
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <Dummy active text="hello" />
+      <button onClick={onClick}>click me</button>
+    </Container>
   );
 }
 
--- Typescript/src/index.tsx
+++ Typescript/src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { lightTheme } from "./theme";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
