af6965a33e15c0653321121a5eef2270b4d597eb
Kim-EunSu <kes73640@gmail.com>
2022-12-07 14:53:22 +0900

    1207

--- CryptoTracker/src/theme.ts
+++ CryptoTracker/src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "black",
+  textColor: "white",
+  accentColor: "red",
 };
