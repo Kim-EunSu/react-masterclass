7ffcf27f53eb8f1ea4ddbdf20334dd943e545d14
Kim-EunSu <kes73640@gmail.com>
2022-12-07 16:48:29 +0900

    1207

--- CryptoTracker/src/routes/Coin.tsx
+++ CryptoTracker/src/routes/Coin.tsx
@@ -1,14 +1,54 @@
+import styled from "styled-components";
 import { useParams } from "react-router";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 interface RouterParams {
-  [key: string]: string | undefined;
   coinId: string;
 }
 
-function Coin() {
-  const { coinId } = useParams<RouterParams>();
+interface RouteState {
+  name: string;
+}
 
-  return <h1>Coin: {coinId}</h1>;
+function Coin() {
+  const [isLoading, setIsLoading] = useState(true);
+  const { coinId } = useParams<"coinId">();
+  // const { state } = useLocation() as RouteState;
+  const location = useLocation();
+  const state = location.state as RouteState;
+
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading.."}</Title>
+      </Header>
+      {isLoading ? <Loader> Loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
