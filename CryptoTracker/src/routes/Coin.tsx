1c56d5e55b257202e3416972921e8ba980af3a6d
Kim-EunSu <kes73640@gmail.com>
2022-12-08 14:28:26 +0900

    1208

--- CryptoTracker/src/routes/Coin.tsx
+++ CryptoTracker/src/routes/Coin.tsx
@@ -1,8 +1,9 @@
-import { Outlet, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -20,6 +21,28 @@ const Header = styled.header`
 const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgb(255 255 255 / 50%);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
 `;
 
 const Loader = styled.span`
@@ -126,6 +149,10 @@ function Coin() {
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
 
+  // useMatch에게 :coinId/price라는 URL에 있는지 확인
+  const priceMathch = useMatch("/:coinId/price");
+  const chartMathch = useMatch("/:coinId/chart");
+
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -179,6 +206,14 @@ function Coin() {
               <span>{priceInfo?.max_supply}</span>
             </OverviewItem>
           </Overview>
+          <Tabs>
+            <Tab isActive={priceMathch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+            <Tab isActive={chartMathch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+          </Tabs>
           <Outlet />
         </>
       )}
