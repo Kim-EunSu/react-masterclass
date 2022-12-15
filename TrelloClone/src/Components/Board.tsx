73b81d82fe5b290e3ac80c76c3ecf28af31030a9
Kim-EunSu <kes73640@gmail.com>
2022-12-15 15:19:57 +0900

    style 추가

--- TrelloClone/src/Components/Board.tsx
+++ TrelloClone/src/Components/Board.tsx
@@ -7,12 +7,20 @@ interface IBoardProps {
   boardId: string;
 }
 
+interface IAreaProops {
+  isDraggingOver: boolean;
+  draggingFromThisWith: boolean;
+}
+
 const Wrapper = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
+  width: 300px;
+  padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
-  min-height: 200px;
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 `;
 
 const Title = styled.h2`
@@ -22,19 +30,40 @@ const Title = styled.h2`
   font-size: 18px;
 `;
 
+// isDraggingOver라는 prop을 받을거라고 말해줌
+const Area = styled.div<IAreaProops>`
+  /* 드래그 해서 올라오면 분홍색,  */
+  /* 드래그 해서 떠날 때면 빨간색 */
+  background-color: ${(props) =>
+    props.isDraggingOver
+      ? "#dfe6e9"
+      : props.draggingFromThisWith
+      ? "#b2bec3"
+      : "transparent"};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
+  padding: 20px;
+`;
+
 //Bodard가 toDos prop을 가지며 droppableId가 필요
 function Board({ toDos, boardId }: IBoardProps) {
   return (
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
+        {(provided, snapshot) => (
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            // string이든 뭐든 존재하기만 하면 True
+            draggingFromThisWith={Boolean(snapshot.draggingFromThisWith)}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
             {toDos.map((toDo, index) => (
               <DragabbleCard key={toDo} toDo={toDo} index={index} />
             ))}
             {provided.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
