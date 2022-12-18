3014682fbb1ab584c0bbcd180a73b34a4986b2c6
Kim-EunSu <kes73640@gmail.com>
2022-12-18 16:02:00 +0900

    1218 slide

--- Nomflixclone/src/Routes/Home.tsx
+++ Nomflixclone/src/Routes/Home.tsx
@@ -3,8 +3,12 @@ import { getMovies } from "../api";
 import { IGetMoviesResult } from "../api";
 import styled from "styled-components";
 import { makeImagePath } from "../utils";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  background-color: black;
+`;
 
 const Loader = styled.div`
   height: 20vh;
@@ -27,6 +31,7 @@ const Banner = styled.div<{ bgPhoto: string }>`
 const Title = styled.h2`
   font-size: 48px;
   margin-bottom: 20px;
+  margin-top: 500px;
 `;
 
 const Overview = styled.div`
@@ -34,11 +39,47 @@ const Overview = styled.div`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 60px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth,
+  },
+};
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies, nowPlaying"],
     getMovies
   );
+
+  const [index, setIndex] = useState(0);
+
+  //   index를 증가시키는 함수
+  const increaseIndex = () => setIndex((prev) => prev + 1);
 
   return (
     <Wrapper>
@@ -46,11 +87,30 @@ function Home() {
         <Loader>Loading...</Loader>
       ) : (
         <>
-          {/* <Banner bgPhoto={data?.results[0].backdrop_path}> */}
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
-            <Title>{data?.results[0].title}</Title>
-            <Overview>{data?.results[0].overview}</Overview>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[1].backdrop_path || "")}
+          >
+            <Title>{data?.results[1].title}</Title>
+            <Overview>{data?.results[1].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              {/* key만 바꿔주고 있음 => key가 변경되면, React.js는 새로운 Row가 만드어졌다고 생각  */}
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                key={index}
+                transition={{ type: "tween", duration: 1 }}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
--- Nomflixclone/src/utils.ts
+++ Nomflixclone/src/utils.ts
@@ -2,5 +2,6 @@
 // format이 제공된다면 format을 사용하고 없으면 원본을 사용
 
 export function makeImagePath(id: string, format?: string) {
+  // 그저 string을 return
   return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
 }
