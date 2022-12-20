d3a97e6b1193de02b735cd9503dde15f86c4c952
Kim-EunSu <kes73640@gmail.com>
2022-12-20 12:54:31 +0900

    urlSearchParameter()

--- Nomflixclone/src/Routes/Search.tsx
+++ Nomflixclone/src/Routes/Search.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Search() {
-  return <div>Search</div>;
+  // 1. keyword에 먼저 접근하므로 keyword를 가져와야함
+  const location = useLocation();
+
+  // 2. keyword 받아오기
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
+
+  return null;
 }
 
 export default Search;
